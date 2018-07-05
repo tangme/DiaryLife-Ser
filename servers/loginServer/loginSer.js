38125cb4d94f73c65de60ac277ede7121e9b7179
tanglv <tangchaofalv@163.com>
2018-07-05 17:45:44 +0800

    更新登录返回数据信息

--- servers/loginServer/loginSer.js
+++ servers/loginServer/loginSer.js
@@ -1,7 +1,7 @@
 const mysql = reqlib('/database/mysqlDriver');
 
 /*查询登录信息*/
-const queryUserSql = `SELECT 1 as result from user t where (t.account=? or t.email=? or t.phone=?) and t.pwd=?`;
+const queryUserSql = `SELECT account,email,phone from user t where (t.account=? or t.email=? or t.phone=?) and t.pwd=?`;
 /*查询电子邮箱、手机号是否已经存在*/
 const checkAccountExistedSql = `SELECT
 				( CASE WHEN t.email = ? THEN 1 ELSE 0 END ) email,
@@ -24,8 +24,8 @@ module.exports = {
 		}
 		let sqlParams = [account,account,account,pwd];
 		return mysql.exe(queryUserSql,sqlParams).then(function(data){
-	        if(!!data && data.length==1 && data[0].result == 1){
-	            return {'code':1,'msg':'登录成功'};
+	        if(!!data && data.length==1){
+	            return {'code':1,'msg':'登录成功','userObj':data[0]};
 	        }else{
 	            return {'code':0,'msg':'帐号或密码错误'};
 	        }
